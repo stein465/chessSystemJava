dc04183e3361081abb0dcfa2614ae3f5cfa548ec
Stein <igorxgabriel@gmail.com>
2020-07-07 17:30:58 -0300

    implementing rook posible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,7 @@ public class ChessMatch {
         Position source = sourcePositon.toPosition ();
         Position target = targetPosition.toPosition ();
         validateSourcePosition(source);
+        validateTargetPosition ( source, target );
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
@@ -62,6 +63,12 @@ public class ChessMatch {
         }
         if (!board.piece ( position ).isThereAnyPossibleMove ()){
             throw new ChessException ( "there is not any possibles moves for the chosen piece" );
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece ( source ).possibleMove ( target )){
+            throw new ChessException ( "chosen piece cannot move to target position" );
         }
     }
 
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,6 +18,53 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat =  new boolean[getBoard ().getRows ()][getBoard ().getColumns ()];
+        Position p = new Position ( 0,0 );
+        //above
+        p.setValues ( position.getRow ()-1, position.getCollumn () );
+        while (getBoard ().positionExists ( p ) && !getBoard ().thereIsAPiece ( p )){           // while there isnt any piece, on line, positions will be turn to true
+            mat[p.getRow ()][p.getCollumn ()] = true;
+            p.setRow ( p.getRow ()-1 );
+        }
+
+        if (getBoard ().positionExists ( p ) && isThereOpponetPiece ( p )){                     // if its stops and position has a opponent piece, poisiton will be true
+            mat[p.getRow ()][p.getCollumn ()] = true;
+        }
+
+        //bellow
+        p.setValues ( position.getRow ()+1, position.getCollumn () );
+        while (getBoard ().positionExists ( p ) && !getBoard ().thereIsAPiece ( p )){           // while there isnt any piece, on line, positions will be turn to true
+            mat[p.getRow ()][p.getCollumn ()] = true;
+            p.setRow ( p.getRow ()+1 );
+        }
+
+        if (getBoard ().positionExists ( p ) && isThereOpponetPiece ( p )){                     // if its stops and position has a opponent piece, poisiton will be true
+            mat[p.getRow ()][p.getCollumn ()] = true;
+        }
+
+
+        //left
+        p.setValues ( position.getRow (), position.getCollumn () -1 );
+        while (getBoard ().positionExists ( p ) && !getBoard ().thereIsAPiece ( p )){           // while there isnt any piece, on line, positions will be turn to true
+            mat[p.getRow ()][p.getCollumn ()] = true;
+            p.setCollumn ( p.getCollumn () -1 );
+        }
+
+        if (getBoard ().positionExists ( p ) && isThereOpponetPiece ( p )){                     // if its stops and position has a opponent piece, poisiton will be true
+            mat[p.getRow ()][p.getCollumn ()] = true;
+        }
+
+        //right
+        p.setValues ( position.getRow (), position.getCollumn () +1 );
+        while (getBoard ().positionExists ( p ) && !getBoard ().thereIsAPiece ( p )){           // while there isnt any piece, on line, positions will be turn to true
+            mat[p.getRow ()][p.getCollumn ()] = true;
+            p.setCollumn ( p.getCollumn () +1 );
+        }
+
+        if (getBoard ().positionExists ( p ) && isThereOpponetPiece ( p )){                     // if its stops and position has a opponent piece, poisiton will be true
+            mat[p.getRow ()][p.getCollumn ()] = true;
+        }
+
+
 
         return mat;
     }
